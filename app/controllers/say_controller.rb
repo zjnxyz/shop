e8b5962a3f1d7a85e673368a1183a54621851c35
zjnxyz <469174375@qq.com>
2013-03-15 12:01:51 +0800

    修改一些东西

--- app/controllers/say_controller.rb
+++ app/controllers/say_controller.rb
@@ -2,14 +2,14 @@ class SayController < ApplicationController
   skip_before_filter :require_login
   def find_p_by_brand
   	@brand=Brand.find(params[:id].to_i)
-  	@products=Product.where("brand_id = ?",params[:id].to_i)
+  	@products=Product.where("brand_id = ? and status = ?",params[:id].to_i,true)
   	@products_group=@products.group_by{|p| p.category}
   	@order=Order.new
   end
 
   def find_p_by_category
   	@category=Category.find(params[:id].to_i)
-  	@products=Product.where("category_id = ?",params[:id].to_i)
+  	@products=Product.where("category_id = ? and status = ?",params[:id].to_i,true)
   	@products_group=@products.group_by{|p| p.brand}
   	@order=Order.new
   end
